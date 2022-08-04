86d30d610a3f33fa4a89028788c7f02ab11a58bc
micz044 <78461335+micz044@users.noreply.github.com>
2022-08-04 22:32:45 +0800

    menambah output

--- michael.java
+++ michael.java
@@ -1,5 +1,7 @@
 public class michael{
   public static void main(String[] args){
+    system.out.println("nama saya adalah michael");
+    system.out.println("ini merupakan Program pertama saya di GitHub!!!")
   
   }
   
